cde009560f8ece9beb0859bc6e837860790b872d
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2022-05-01 01:41:25 +0600

    final done

--- src/components/Inventory/InventoryCase.js
+++ src/components/Inventory/InventoryCase.js
@@ -3,17 +3,17 @@ import { Button, Card, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const InventoryCase = ({ children, item }) => {
-    const {_id, itemName, description, supplierName, imgURL, price, quantity } = item;
+    const { _id, itemName, description, supplierName, imgURL, price, quantity } = item;
     return (
         <Col>
             <Card>
                 <Card.Img variant="top" src={imgURL} />
                 <Card.Body>
-                    <Card.Title>Name: {itemName}</Card.Title>
+                    <Card.Title>{itemName}</Card.Title>
+                    <Card.Text>Writer: {supplierName}</Card.Text>
                     <Card.Text>Description: {description}</Card.Text>
                     <Card.Text>Price: {price}</Card.Text>
                     <Card.Text>Quantity: {quantity} </Card.Text>
-                    <Card.Text>Writer Name: {supplierName}</Card.Text>
                 </Card.Body>
                 <Button variant='success' className='my-3' as={Link} to={`/inventory/${_id}`}>Manage</Button>
                 {children}
